9c3418078961dac003a476e0bf69fdcb1aa86189
tin <tinsatthu1988@gmail.com>
2022-03-16 19:17:06 +0700

    favorite book

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as httpClient;
+import 'dart:convert';
 
 class Book with ChangeNotifier {
   int id;
@@ -31,8 +33,34 @@ class Book with ChangeNotifier {
     'isFavorite': isFavorite,
   };
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
-    notifyListeners();
+  Future<void> toggleFavoriteStatus() {
+    Uri url = Uri.parse('http://10.0.2.2:8081/api/books/$id');
+    Map<String, String> headers = {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    };
+
+    return httpClient.put(url, headers: headers, body: json.encode({
+      'title': title,
+      'author': author,
+      'category': category,
+      'description': description,
+      'unitPrice': unitPrice,
+      'imageUrl': imageUrl,
+      'favorite': !isFavorite,
+      'unitsInStock': 100,
+      'active': true,
+    })).then((response) {
+      if (response.statusCode == 200) {
+        isFavorite = !isFavorite;
+        notifyListeners();
+      } else {
+        try {
+          print(json.decode(response.body));  
+        } on FormatException catch (e) {
+          print('Message return is not a valid JSON format');
+        }
+      }
+    }).catchError((error) => throw error);
   }
 }
